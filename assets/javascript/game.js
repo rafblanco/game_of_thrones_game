be103672538cac6c3831c0e7e0b44e5b7b367cb5
Rafael Blanco <rafaelblanco@Rafaels-MBP-2.fios-router.home>
2018-10-07 17:47:40 -0400

    Final logic and styling to the game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
     var players = {
         "jonsnow": {
             healthpower: 100,
@@ -30,74 +30,65 @@ $(document).ready(function() {
         }
     };
 
-
-    // just add the attack power to the your character and to the enemies 
-    // var count;
+    //Global variables
     var yourCharacter = "";
     var enemy = "";
-    // var yourAP = 0;
-    // var defenderHP = 0;
 
     var firstCharacter = false;
     var secondCharacter = false;
 
-    // Give each player their attributes
-    // $.each(players, function() {
-    //     $.each(this, function(index, healthpower){
-    //         console.log(healthpower)
-    //     });
-    //         //$("#" + index).html(index[healthpower]);
-    //         //console.log( healthpower);
-    //         //console.log("------")
-    //         //console.log(players["jonsnow"].healthpower)
-    // });
+    // Give each player their health attributes
     $("#jonsnow .health").html(players["jonsnow"].healthpower);
     $("#kingslayer .health").html(players["kingslayer"].healthpower);
     $("#thehound .health").html(players["thehound"].healthpower);
     $("#oberyn .health").html(players["oberyn"].healthpower);
 
 
+    // Fighters
     $(".fighter").on("click", function () {
-        if (firstCharacter === false) {
-            firstCharacter = true;
-            $("#your-character").append(this);
-            yourCharacter = this.id;
-            $("#enemies").append($("#starting-section"));
-            $("#starting-section > div").css({"background-color": "red", "border": "1px solid black"});
-        } else {
-            secondCharacter = true;
-            $("#defender").append(this);
-            enemy = this.id;
-        }
         if (firstCharacter === true && secondCharacter === true) {
             return false;
+        } else {
+            if (firstCharacter === false) {
+                firstCharacter = true;
+                $("#your-character").append(this);
+                yourCharacter = this.id;
+                $("#enemies").append($("#starting-section"));
+                $("#starting-section > div").css({ "background-color": "red", "border": "1px solid black" });
+            } else {
+                secondCharacter = true;
+                $("#defender").append(this);
+                enemy = this.id;
+            }
         }
     });
 
+    // Attack Button
     $("#attack-button").on("click", function () {
         if (firstCharacter === true && secondCharacter === true) {
+            $("#display").html("")
             // Your character attacks defender and displays it
             players[enemy].healthpower -= players[yourCharacter].attackpower;
-            $("#"+enemy).find(".health").html(players[enemy].healthpower);
+            $("#" + enemy).find(".health").html(players[enemy].healthpower);
             $("#your-attack").html("You have attacked " + enemy + " for " + players[yourCharacter].attackpower + " damage.");
 
             // Enemy counterattacks back and displays it
             players[yourCharacter].healthpower -= players[enemy].counterattack;
-            $("#"+ yourCharacter).find(".health").html(players[yourCharacter].healthpower);
+            $("#" + yourCharacter).find(".health").html(players[yourCharacter].healthpower);
             $("#enemy-attack").html(enemy + " attacked you back for " + players[enemy].counterattack + " damage.");
             // Your attack goes up by the base attack
             players[yourCharacter].attackpower += players[yourCharacter].baseattack;
         }
         if (players[enemy].healthpower <= 0) {
             secondCharacter = false;
-            enemy = "";
             $("#defender").html("");
             $("#display").html("You have defeated " + enemy);
-        } 
-        if (players[yourCharacter].healthpower <= 0){
+            enemy = "";
+        }
+        if (players[yourCharacter].healthpower <= 0) {
             $("#your-character").html("");
-            $("#display").html("You have brought a great deal of shame to the " + players[yourCharacter].family);
-
+            $("#loss-display").html("You have brought a great deal of shame to the " + players[yourCharacter].family);
+            $("<section>").hide();
         }
     });
 });
